4265c8fe22421f7305364065771744082867bc07
Lingfeng <2354833@qq.com>
2019-06-27 22:48:42 +0800

    Damn Cat！

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,14 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(age < 0 || name == ""|| name==null){
+            return INVALID_CAT;
+        }else{
+            return new Cat(name,age);
+        }
+
+    }
 
     public String getName() {
         return name;
@@ -27,4 +34,10 @@ public class Cat {
     public int getAge() {
         return age;
     }
+
+    private Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
 }

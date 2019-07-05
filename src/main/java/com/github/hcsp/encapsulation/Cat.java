63f58546430e23c0cc7c4f627e42e407204eeff0
huangjiaxi <huangjiaxi@oak-inc.com>
2019-07-05 13:32:37 +0800

    public static factory method

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.encapsulation;
 
+import java.util.Objects;
+
 public class Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
@@ -18,7 +20,18 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if( age < 0 || Objects.equals("",name) || name == null){
+            return INVALID_CAT;
+        }else{
+            return new Cat(name,age);
+        }
+    }
+
+    Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

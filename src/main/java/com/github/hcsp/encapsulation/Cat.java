2a49d27bd3be362f8fa0ecdf7db8d889ef7a85d0
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-07-10 10:40:34 +0800

    Update Cat.java (#22)
    Update Cat.java (#22)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -4,21 +4,22 @@ public class Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
     private int age;
-
-    /**
-     * 创建一只猫的工厂方法。当传入的参数无效，即：
-     *
-     * <p>1. age小于0 2. name是空字符串或者null时
-     *
-     * <p>返回预先创建好的{@link #INVALID_CAT}；
-     *
-     * <p>否则，返回一只新创建的猫
-     *
-     * @param age 年龄
-     * @param name 名字
-     * @return 创建的猫
-     */
-    public static Cat newCat(String name, int age) {}
+    
+    
+    public static Cat newCat(String name, int age) {
+        if(age < 0 || name == null || "".equals(name)) {
+            // 工厂方法可以在内部很灵活的做一些判空处理等等 本例中如果age小于0或者name是空字符串或者null时 返回预先创建好的INVALID_CAT
+            return INVALID_CAT;
+        }else {
+            return new Cat(name, age);
+        }
+    }
+    
+    // 构造器要私有化
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

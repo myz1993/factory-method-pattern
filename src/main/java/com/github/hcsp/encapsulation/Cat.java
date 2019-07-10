0d8d09bf59aaae1748e74a69d1d9580e8c4a8bbd
Li-saltair <huagongyilby@126.com>
2019-07-10 16:24:32 +0800

    Cat工厂 (#23)
    Cat工厂 (#23)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,19 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(name == null || name.equals("") || age<0){
+            return INVALID_CAT;
+        } else {
+
+            return new Cat(name,age);
+        }
+    }
+
+    private Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

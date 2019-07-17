103ac75a8461e72d1072c115e73fb2065144ba10
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-07-17 17:23:10 +0800

    工厂方法模式实战 (#28)
    工厂方法模式实战 (#28)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,17 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (name == null || name.isEmpty() || age<0){ //  缺乏"age < 0"这个条件的时候，mvn clean test也成功
+            return INVALID_CAT;
+        }
+        return new Cat(name,age);
+    }
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

9755a3f4e974bbdd1441311a964175aaf4cce274
胡凯波 <1069977596@qq.com>
2019-07-28 19:26:57 +0800

    体会静态工厂方法的作用

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,9 @@ public class Cat {
     private String name;
     private int age;
 
+    public Cat(String name, int age) {
+    }
+
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -18,7 +21,12 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age<0||name==null){
+            return INVALID_CAT;
+        }return newCat(name,age);
+
+    }
 
     public String getName() {
         return name;

2ec0f90c99cb60a91bfa3271c3a8a65dd29d0a11
fei tang <497045233@qq.com>
2019-08-10 09:42:03 +0800

    工厂方法模式 (#38)
    工厂方法模式 (#38)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,9 +1,15 @@
 package com.github.hcsp.encapsulation;
 
+
 public class Cat {
+
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
     private int age;
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
@@ -14,11 +20,16 @@ public class Cat {
      *
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || name.isEmpty()) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
 
     public String getName() {
         return name;
@@ -28,3 +39,5 @@ public class Cat {
         return age;
     }
 }
+
+

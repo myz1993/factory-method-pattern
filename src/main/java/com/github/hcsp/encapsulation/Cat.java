8f1cef4e5a2dad138929412bbe61b1ff474223fd
perfectLipeng <49055265+perfectLipeng@users.noreply.github.com>
2019-09-10 11:31:17 +0800

    工厂方法模式实战 (#54)
    工厂方法模式实战 (#54)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -7,18 +7,29 @@ public class Cat {
 
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
-     *
+     * <p>
      * <p>1. age小于0 2. name是空字符串或者null时
-     *
+     * <p>
      * <p>返回预先创建好的{@link #INVALID_CAT}；
-     *
+     * <p>
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (name == null || name.equals("") || age < 0) {
+            return INVALID_CAT;
+        } else {
+            return new Cat(name, age);
+        }
+    }
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

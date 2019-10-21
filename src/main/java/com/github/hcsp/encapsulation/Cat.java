6684ac5e2724f630420bcfe129bdebfaad90c8cd
JN0916 <56193588+JN0916@users.noreply.github.com>
2019-10-21 22:53:02 +0800

    静态工厂方法 (#78)
    静态工厂方法 (#78)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,16 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (name == null || name.length() == 0 || age < 0)
+            return INVALID_CAT;
+        return new Cat(name, age);
+    }
+
+    private Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

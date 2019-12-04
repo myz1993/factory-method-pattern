10f2d4c1903e8408241013633fc42800fd6bd95a
zhangsht <z429882801@qq.com>
2019-12-04 17:57:37 +0800

    Java面向对象初步练习：工厂方法模式实战

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -24,7 +24,7 @@ public class Cat {
      * @return 创建的猫
      */
     public static Cat newCat(String name, int age) {
-        if (age < 0.2 && name.isEmpty())return INVALID_CAT;
+        if (age < 0 || name.isEmpty())return INVALID_CAT;
         return new Cat(name,age);
     }
 

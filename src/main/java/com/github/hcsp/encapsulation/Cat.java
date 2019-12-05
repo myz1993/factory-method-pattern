7933f5bb06e6c5d7d918dbbdfa9b8d5f03ec73f9
tan661 <lucky.tanduzhi@gmail.com>
2019-12-05 20:07:04 +0800

    感受工厂方法的好处 (#94)
    感受工厂方法的好处 (#94)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,17 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(age < 0 || name == null  || name.isEmpty()) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

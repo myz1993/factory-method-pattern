79ad3db746a13d656aca3d46d36fa156059d0554
Bing Qin <qinbing08@gmail.com>
2020-01-05 10:13:57 +0800

    工厂方法实战 (#122)
    工厂方法实战 (#122)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,9 +1,16 @@
 package com.github.hcsp.encapsulation;
 
-public class Cat {
+public class
+Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
     private int age;
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
 
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
@@ -14,11 +21,16 @@ public class Cat {
      *
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if ("".equals(name) || null == name || age < 0) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
 
     public String getName() {
         return name;

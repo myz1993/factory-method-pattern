96549a03d433702a7acdb3842cf8005b7e0363de
Felix Mi <41504852+mm123mm123@users.noreply.github.com>
2020-01-09 22:53:30 +0800

    构造器封装 (#126)
    构造器封装 (#126)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,7 +18,16 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    Cat(String name,int age){this.name=name;this.age=age;}
+
+
+    public static Cat newCat(String name, int age) {
+
+        if(name == null||age<0 || name.equals("") ){
+            return INVALID_CAT ;}
+        else{
+            return new Cat(name,age);}
+    }
 
     public String getName() {
         return name;

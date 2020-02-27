c003a7186241f75e8958d6aa6351cbb78f9b71ef
ARKbreeze <61083620+ARKbreeze@users.noreply.github.com>
2020-02-27 21:54:47 +0800

    完成工厂模式 (#149)
    完成工厂模式 (#149)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,11 @@ public class Cat {
     private String name;
     private int age;
 
+    private Cat(String invalid_cat, int i) {
+        this.name = invalid_cat ;
+        this.age = i ;
+    }
+
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -18,7 +23,15 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(age<0 || name == null || "".equals(name) ){
+            return INVALID_CAT;
+        }
+
+        return new Cat(name,age);
+
+
+    }
 
     public String getName() {
         return name;

5a29998a4895365979bd7e36ad011385ce9a2533
luyi <luyi95@foxmail.com>
2020-03-03 16:44:47 +0800

    使用静态工厂方法创建Cat对象

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,11 @@ public class Cat {
     private String name;
     private int age;
 
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -18,7 +23,13 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || name.isEmpty()) {
+            return INVALID_CAT;
+        } else {
+            return new Cat(name, age);
+        }
+    }
 
     public String getName() {
         return name;

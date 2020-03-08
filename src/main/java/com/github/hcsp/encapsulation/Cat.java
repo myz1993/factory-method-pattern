c3469514b4fa70865caa485610feff910b0076ed
举高高 <463927289@qq.com>
2020-03-08 21:53:18 +0800

    工厂方法模式实战 (#158)
    工厂方法模式实战 (#158)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -4,7 +4,10 @@ public class Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
     private int age;
-
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -14,11 +17,17 @@ public class Cat {
      *
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || "".equals(name)) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
 
     public String getName() {
         return name;

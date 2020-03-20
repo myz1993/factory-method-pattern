4f88e8892334a6963f20a3610bc07718659cf05f
XinleZhou <2320789321>
2020-03-20 13:51:52 +0800

    创建一只猫的工厂方法

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -14,22 +14,24 @@ public class Cat {
      *
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
     public static Cat newCat(String name, int age) {
-        if(age<0||name==null||name.equals("")){
+        if (age < 0 || name == null || name.equals("")) {
             return INVALID_CAT;
-        }else{
+        } else {
             return new Cat(name, age);
         }
 
     }
-    private Cat(String name,int age){
-        this.name=name;
-        this.age=age;
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
+
     public String getName() {
         return name;
     }

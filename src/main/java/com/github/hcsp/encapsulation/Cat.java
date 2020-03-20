9d3d6b2dc95a46b23108dd2f100a33251a53a853
liulian1999 <61763395+liulian1999@users.noreply.github.com>
2020-03-20 13:28:08 -0700

    pattern (#164)
    pattern (#164)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -18,11 +18,24 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(age < 0 || name == null || name.equals("")){
+            return INVALID_CAT;
+        }
+        else {
+            return new Cat(name, age);
+        }
+    }
+    private Cat (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
 
     public String getName() {
         return name;
     }
+
 
     public int getAge() {
         return age;

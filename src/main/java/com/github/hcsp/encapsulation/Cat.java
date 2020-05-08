ac91aff546301e74cff3fa6792e65dfd391b784e
yujie-zhang <907420318@qq.com>
2020-05-08 20:42:13 +0800

    1 (#183)
    1 (#183)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.encapsulation;
 
 public class Cat {
-    private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
+    private static final Cat INVALID_CAT = new Cat("Invalid cat",-1);
     private String name;
     private int age;
 
@@ -18,7 +18,12 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || name.isEmpty() ) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
 
     public String getName() {
         return name;
@@ -26,5 +31,9 @@ public class Cat {
 
     public int getAge() {
         return age;
+    }
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }

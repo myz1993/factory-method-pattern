1e33881af57e610ec4a582f3e4adf68ee6563799
kongmu373 <62830983+kongmu373@users.noreply.github.com>
2020-05-22 00:12:51 +0800

    创建一只猫的工厂方法。 (#185)
    创建一只猫的工厂方法。 (#185)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,7 @@ public class Cat {
     private String name;
     private int age;
 
+
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -18,7 +19,17 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || "".equals(name)) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
+
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() {
         return name;

0934ae58a87933e86267f1a7e62a6b54fad6e61a
godvvvvvv <lgj88508@163.com>
2020-06-29 16:52:22 +0800

    工厂方法

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,7 +5,7 @@ public class Cat {
     private String name;
     private int age;
 
-    public Cat(String name, int age) {
+    private Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
